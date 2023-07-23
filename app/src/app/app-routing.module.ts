97c11bda80e507c5a470bb4149de1fa67d713a8c
noxbike <noxbike@gmail.com>
2023-07-23 09:12:06 +0400

    add card for statistic layout

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateEstimateInvoiceComponent } from './layout/create-estimate-invoice/create-estimate-invoice.component';
 import { HomeComponent } from './layout/home/home.component';
 import { SettingComponent } from './layout/setting/setting.component';
+import { StatistiqueViewComponent } from './layout/statistique-view/statistique-view.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'invoices', component: CreateEstimateInvoiceComponent},
   {path: 'settings', component: SettingComponent},
+  {path: 'statistics', component: StatistiqueViewComponent}
 ];
 
 @NgModule({
--- app/src/app/layout/statistique-view/statistique-view.component.ts
+++ app/src/app/layout/statistique-view/statistique-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-statistique-view',
   templateUrl: './statistique-view.component.html',
   styleUrls: ['./statistique-view.component.scss'],
   standalone: true,
-  imports: [MatButtonModule]
+  imports: [MatButtonModule, MatCardModule]
 })
 export class StatistiqueViewComponent {
 
